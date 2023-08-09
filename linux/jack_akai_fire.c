676a007aae30e5c8e44761fe549882723ffafaea
Tom Schouten <tom@zwizwa.be>
2023-08-09 19:55:26 +0200

    input -> output akai fire button

--- linux/jack_akai_fire.c
+++ linux/jack_akai_fire.c
@@ -9,6 +9,8 @@
 #include <jack/jack.h>
 #include <jack/midiport.h>
 
+#include "pbuf.h"
+
 /* AKAI FIRE */
 
 
@@ -17,13 +19,6 @@ static jack_port_t *midi_out = NULL;
 static jack_port_t *midi_in = NULL;
 static jack_client_t *client = NULL;
 
-// Send midi data out over a jack port.
-static inline void send_midi(void *out_buf, jack_nframes_t time,
-                             const void *data_buf, size_t nb_bytes) {
-    //LOG("%d %d %d\n", frames, time, (int)nb_bytes);
-    void *buf = jack_midi_event_reserve(out_buf, time, nb_bytes);
-    if (buf) memcpy(buf, data_buf, nb_bytes);
-}
 
 #define BPM_TO_PERIOD(sr,bpm) ((sr*60)/(bpm*24))
 
@@ -31,13 +26,54 @@ static inline void send_midi(void *out_buf, jack_nframes_t time,
 #define COLS 16
 uint8_t pads[ROWS][COLS] = {};
 
-void pad(uint8_t row, uint8_t col) {
-    pads[row][col] ^= 1;
-    LOG("%d %d = %d\n", row, col, pads[row][col]);
+// Numbers are in hex, same as the doc
+// https://blog.segger.com/decoding-the-akai-fire-part-1/
+#define PAD_OFFSET 0x36
+
+uint8_t pad_nb(uint8_t row, uint8_t col) {
+    return PAD_OFFSET + col + row * 16;
 }
 
-static int process (jack_nframes_t nframes, void *arg) {
+#define SYSEX_HEADER_INIT { \
+    0xF0, /* System Exclusive */ \
+    0x47, /* Akai Manufacturer ID (see the MMA site for a list) */ \
+    0x7F, /* The All-Call address */ \
+    0x43, /* Sub-ID byte #1 identifies "Fire" product */ \
+    0x65, /* Sub-ID byte #2 identifies the command */ \
+}
 
+const uint8_t sysex_header[] = SYSEX_HEADER_INIT;
+
+#define SYSEX_FOOTER_BYTE 0xF7
+
+void pad_event(void *out_buf, uint8_t row, uint8_t col) {
+    pads[row][col] ^= 1;
+    LOG("%d %d = %d\n", row, col, pads[row][col]);
+    int on = pads[row][col];
+    uint8_t data[] = {
+        0x00, 0x04, // size in 7-7
+        col + row * 16,   // pad nb
+        on * 0x40, // r
+        on * 0x40, // g
+        on * 0x40, // b
+    };
+    int nb = sizeof(sysex_header) + sizeof(data) + 1;
+    uint8_t *buf = jack_midi_event_reserve(out_buf, 0 /*time*/, nb);
+    if (buf) {
+        memcpy(buf, sysex_header, sizeof(sysex_header));
+        buf += sizeof(sysex_header);
+        memcpy(buf, data, sizeof(data));
+        buf += sizeof(data);
+        buf[0] = SYSEX_FOOTER_BYTE;
+    }
+    else {
+        ERROR("Can't reserve %d midi bytes\n", nb);
+    }
+}
+
+
+static int process (jack_nframes_t nframes, void *arg) {
+    void *midi_out_buf = jack_port_get_buffer(midi_out, nframes);
     void *midi_in_buf  = jack_port_get_buffer(midi_in, nframes);
     jack_nframes_t n = jack_midi_get_event_count(midi_in_buf);
     for (jack_nframes_t i = 0; i < n; i++) {
@@ -53,11 +89,11 @@ static int process (jack_nframes_t nframes, void *arg) {
             else if (msg[0] == 0x90) { // Note on channel 0
                 // sprintf(buf, "on %d %d;\n", msg[1], msg[2]);
                 uint8_t note = msg[1];
-                if (note >= 54) {
-                    uint8_t col = note - 54;
+                if (note >= PAD_OFFSET) {
+                    uint8_t col = note - PAD_OFFSET;
                     uint8_t row = col / 16;
                     col -= row * 16;
-                    pad(row, col);
+                    pad_event(midi_out_buf, row, col);
                 }
             }
             else if (msg[0] == 0x80) { // Note off channel 0
