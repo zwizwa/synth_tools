11b3400e6e2df2454a7a72ecddb43dbbd98c9809
Tom Schouten <tom@zwizwa.be>
2024-02-25 10:23:28 -0500

    hub2: client, port iteration

--- rs_linux/src/hub2.rs
+++ rs_linux/src/hub2.rs
@@ -2,9 +2,10 @@
 // This is heavily inspired by a2jmidid.
 
 extern crate alsa;
-use alsa::seq::{Addr, Seq};
+use alsa::seq::{ClientIter, PortIter, Seq};
 // use alsa::Direction;
 use std::ffi::CString;
+use std::{thread, time};
 
 fn main() {
     println!("hub2.rs");
@@ -12,8 +13,24 @@ fn main() {
     let s = Seq::open(None, None /* DUPLEX */, false).unwrap();
     s.set_client_name(&*CString::new("hub2").unwrap()).unwrap();
 
-    let a = Addr { client: 0, port: 0 };
-    let i = s.get_any_port_info(a).unwrap();
-    // It has fmt::Debug trait
-    println!("port: {:?}", i);
+    let clients: Vec<_> = ClientIter::new(&s).collect();
+    for a in &clients {
+        let ports: Vec<_> = PortIter::new(&s, a.get_client()).collect();
+        println!("{:?}:", a);
+        for p in &ports {
+            println!("  {:?}", p);
+        }
+    }
+
+    // let a = Addr {
+    //     client: 24,
+    //     port: 0,
+    // };
+    // let i = s.get_any_port_info(a).unwrap();
+    // println!("port: {:?}", i);
+
+    //loop {
+    println!(".");
+    thread::sleep(time::Duration::from_millis(1000));
+    //}
 }
