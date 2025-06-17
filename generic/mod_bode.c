bfa45e7e1f2b92d427f0b8520bc005926f2568ed
Tom Schouten <tom@zwizwa.be>
2025-06-17 09:49:03 -0400

    mod_bode.c

--- generic/mod_bode.c
+++ generic/mod_bode.c
@@ -14,7 +14,6 @@ typedef float complex_float[2];
 struct bode_vec {
     float db;
     float phase;
-    float x_axis;
 };
 struct bode {
     int offset_start;
@@ -27,54 +26,6 @@ struct bode {
     struct bode_vec *vec;
 };
 
-/* Compute the X-axis of a semilog plot.  We will not do any
-   resampling.  Instead we compute the X-axis points corresponding to
-   each sample point.
-
-   The x_axis is in scaled units:
-   - 0 = left of diagram
-   - 1 = right of diagram
-
-   offset_start / f_0     left   0.0
-   offset_end   / f_max   right  1.0
-
-   The x coordinate will then be:
-
-   log (f / f_0) / log(f_max / f_0)
-
-   What might be more useful is to normalize this to decades, since
-   that will also be the background grid used in the SVG.  That would
-   be:
-
-   log (f / f_0) / log(10)
-
-   0    20
-   1   200
-   2  2000
-   3 20000
-
-*/
-static void bode_compute_semilog_axis(struct bode *bode) {
-    float f_0 = bode->f_0;
-    // float f_max = bode->f_max;
-    // float scale = 1.0f / logf(f_max / f_0);
-    float scale = 1.0f / logf(10);
-    float f = bode->f_0;
-    for (int n=bode->offset_start; n<=bode->offset_end; n++) {
-
-        float x = scale * logf(f / f_0);
-        if (0) {
-            LOG("n=%d x=%f f=%f, db=%f ph=%f\n",
-                n,
-                x,
-                f,
-                bode->vec[n].db,
-                bode->vec[n].phase);
-        }
-        f += bode->f_step;
-        bode->vec[n].x_axis = x;
-    }
-}
 
 static void bode_fft_to_spectrum(struct bode *bode, const complex_float *fft1) {
 
@@ -102,7 +53,6 @@ static void bode_fft_to_spectrum(struct bode *bode, const complex_float *fft1) {
     float f_range = ((float)bode->offset_end) / ((float)bode->offset_start);
     bode->f_max = bode->f_0 * f_range;
 
-    bode_compute_semilog_axis(bode);
 }
 
 /* Compute the SVG path string needed to plot the spectrum.
@@ -113,18 +63,33 @@ static void bode_fft_to_spectrum(struct bode *bode, const complex_float *fft1) {
    ...
 
 */
-static int bode_svg_path_db_inner(const struct bode *bode, char sep,
+static int bode_svg_path_db_inner(const struct bode *bode,
+                                  float pixels_per_decade,
+                                  char sep,
                                   char *dst, int room) {
+
+
+    /* This is the data we're fitting onto the grid. */
+    float decade_left  = log10f(20);
+    float decade_right = log10f(bode->samplerate/2);
+
     char tag = 'M';
     int size = 0;
     for (int n=bode->offset_start; n<=bode->offset_end; n++) {
-        int chunk =
-            snprintf(
-                dst, room,
-                "%c %f %f%c",
-                tag,
-                bode->vec[n].x_axis,
-                bode->vec[n].db, sep);
+        float f = ((float)n) * bode->f_step;
+        float decade = log10f(f);
+
+        /* The units are db=pixels, and parameterized number of pixels
+           per decade.  A square "bode square" (the 20dB/decade first
+           order rolloff) would be 20, but a more natural scale is
+           60 which is a 3:1 aspect ratio for the semilog grid. */
+        float x = decade * pixels_per_decade;
+        float y = bode->vec[n].db;
+
+        /* Here x,y are in decade, db coordinates.  It seems simpler
+           to convert them to SVG coordiates here, given the box
+           size. */
+        int chunk = snprintf(dst, room, "%c %f %f%c", tag, x, y, sep);
         // LOG("chunk = %d\n", chunk);
         tag = 'L';
         if (dst != NULL) {
@@ -136,12 +101,14 @@ static int bode_svg_path_db_inner(const struct bode *bode, char sep,
     return size;
 
 }
-static char *bode_svg_path_db(const struct bode *bode, char sep) {
-    int size = bode_svg_path_db_inner(bode, sep, NULL, 0);
+static char *bode_svg_path_db(const struct bode *bode,
+                              float pixels_per_decade,
+                              char sep) {
+    int size = bode_svg_path_db_inner(bode, pixels_per_decade, sep, NULL, 0);
     // LOG("size = %d\n", size);
     int room = size + 1;
     char *buf = malloc(room);
-    bode_svg_path_db_inner(bode, sep, buf, room);
+    bode_svg_path_db_inner(bode, pixels_per_decade, sep, buf, room);
     // LOG("buf:\n%s\n", buf);
     return buf;
 }
