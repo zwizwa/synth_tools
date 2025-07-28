5922d20e7407bebc0f9a14c7257d5adcfa0fb29d
Tom Schouten <tom@zwizwa.be>
2025-07-28 17:17:50 -0400

    GVar works

--- hs/synth-tools.hs
+++ hs/synth-tools.hs
@@ -47,38 +47,45 @@ instance DSL Eval where
 
 class CompState a where
 
-data Comp t = Add (Comp t) (Comp t)
-            | ConstI Int
-            | ConstF Float
-            | SVar
-            | Signal (Comp t)
-  deriving (Show, Functor)
 
-data CompNode s = GAdd s s
-                | GLitI Int
-                | GLitF Float
-                | GSVar
-                deriving (Show)
+data Number = I Int | F Float
+  deriving (Show)
+
+data Stx = Add Stx Stx
+         | Const Number
+         | Var
+         | Signal Stx Stx Stx
+  deriving (Show)
+
+data StxNode s = GAdd s s
+               | GConst Number
+               | GVar
+               | GSignal s s s
+               deriving (Show)
 
 -- Not clear how to use generic Foldable, Traversable.  Just make it explicit.
-instance MuRef (Comp t) where
-  type DeRef (Comp t) = CompNode
-  mapDeRef f (ConstI v) = pure $ GLitI v
-  mapDeRef f (ConstF v) = pure $ GLitF v
-  mapDeRef f (Add a b) = GAdd <$> f a <*> f b
-  mapDeRef f SVar      = pure $ GSVar
+instance MuRef Stx where
+  type DeRef Stx = StxNode
+  mapDeRef f (Const v)      = pure $ GConst v
+  mapDeRef f (Add a b)      = GAdd <$> f a <*> f b
+  mapDeRef f Var            = pure $ GVar
+  mapDeRef f (Signal v s o) = GSignal <$> f v <*> f s <*> f o
+  
   
 -- instance NewVar (Comp t) where
   
 
-instance DSL Comp where
-  constI = ConstI
-  constF = ConstF
-  add = Add
+data Comp t = Comp Stx
+  deriving (Show, Functor)
 
-  signal init update = Signal out where
-    -- We can just tie the knot here
-    (state, out) = update $ SVar
+instance DSL Comp where
+  constI = Comp . Const . I
+  constF = Comp . Const . F
+  add (Comp a) (Comp b) = Comp $ Add a b
+
+  signal init update = Comp $ Signal var state out where
+    var = Var
+    (Comp state, Comp out) = update $ Comp var
 
   -- signal (Comp update) (Comp init) = Comp $ sig init where
   --   sig s = Cons v $ sig s' where
@@ -99,8 +106,15 @@ testEval = do
 
 testComp = do
   let s = (constI 1) :: Comp Int
-      s2 = add s s
+      Comp s2 = add s s
+      Comp s3 = signal 0 (\s -> (add s $ constI 1, s))
+      
   putStrLn $ show s2
   s2' <- reifyGraph s2
   putStrLn $ show $ s2'
+
+  s3' <- reifyGraph s3
+  putStrLn $ show $ s3'
+
+
   
