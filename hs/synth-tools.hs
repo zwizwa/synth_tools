27feabd79550b420ff9afa6ee4318d9960525aa3
Tom Schouten <tom@zwizwa.be>
2025-07-28 18:22:07 -0400

    just use streams to represent init values for now (semantics: sample the first element)

--- hs/synth-tools.hs
+++ hs/synth-tools.hs
@@ -9,7 +9,7 @@
 {-# LANGUAGE DeriveFunctor, DeriveAnyClass #-}
 {-# LANGUAGE TypeFamilies #-} -- For type level functions
 {-# LANGUAGE ExistentialQuantification #-} -- For state hiding in Comp
-
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 import Data.Stream
 import Data.Functor
@@ -18,10 +18,11 @@ import Prelude hiding (take, const)
 
 import Data.Reify
 
+class DSLConst r t where
+  const :: t -> r t
+
 class DSL r where
-  constI  :: Int -> r Int
-  constF  :: Float -> r Float
-  signal  :: s -> (r s -> (r s, r t)) -> r t
+  signal  :: r s -> (r s -> (r s, r t)) -> r t
   add     :: Num t => r t -> r t -> r t
   --lam    :: (r a -> r b) -> r (a -> b)
   --app    :: r (a -> b) -> r a -> r b
@@ -33,59 +34,71 @@ instance Applicative Eval where
   pure = Eval . pure
   (Eval f) <*> (Eval a) = Eval $ f <*> a
 
+instance DSLConst Eval Int where
+  const = Eval . pure
+
 instance DSL Eval where
-  constI = pure
-  constF = pure
   add = liftA2 (+)
   signal init update = v where
-    -- We can just tie the knot here
-    (Eval s, v) = update $ Eval $ Cons init s
+    -- Note that the initial value is encoded as a stream where we
+    -- sample the first value.  This is done in order to avoid having
+    -- to represent both scalars and streams in the language which
+    -- really complicates things.  In the Comp language this can be
+    -- expressed more directly.
+    Eval (Cons init0 _) = init
+    
+    -- Given init0 we can just tie the knot
+    (Eval s, v) = update $ Eval $ Cons init0 s
 
 
 -- Comp has phantom type to be able to implement the DSL.
 
 
-class CompState a where
-
-
 data Number = I Int | F Float
+  deriving (Show)
+
+data VarType = State
   deriving (Show)
 
 data Stx = Add Stx Stx
          | Const Number
-         | Var
-         | Signal Stx Stx Stx
+         | Var VarType
+         | Signal Stx Stx Stx Stx
   deriving (Show)
 
 data StxNode s = GAdd s s
                | GConst Number
-               | GVar
-               | GSignal s s s
+               | GVar VarType
+               | GSignal s s s s
                deriving (Show)
 
 -- Not clear how to use generic Foldable, Traversable.  Just make it explicit.
 instance MuRef Stx where
   type DeRef Stx = StxNode
-  mapDeRef f (Const v)      = pure $ GConst v
-  mapDeRef f (Add a b)      = GAdd <$> f a <*> f b
-  mapDeRef f Var            = pure $ GVar
-  mapDeRef f (Signal v s o) = GSignal <$> f v <*> f s <*> f o
+  mapDeRef f (Const v)        = pure $ GConst v
+  mapDeRef f (Add a b)        = GAdd <$> f a <*> f b
+  mapDeRef f (Var t)          = pure $ GVar t
+  mapDeRef f (Signal i v s o) = GSignal <$> f i <*> f v <*> f s <*> f o
   
   
 -- instance NewVar (Comp t) where
   
+instance DSLConst Comp Int where
+  const = Comp . Const . I
 
 data Comp t = Comp Stx
   deriving (Show, Functor)
 
+--instance DSLConst Comp where
+--  const = Comp . Pure 
+
 instance DSL Comp where
-  constI = Comp . Const . I
-  constF = Comp . Const . F
   add (Comp a) (Comp b) = Comp $ Add a b
 
-  signal init update = Comp $ Signal var state out where
-    var = Var
+  signal (Comp init) update = Comp $ Signal init var state out where
+    var = Var State
     (Comp state, Comp out) = update $ Comp var
+
 
   -- signal (Comp update) (Comp init) = Comp $ sig init where
   --   sig s = Cons v $ sig s' where
@@ -98,16 +111,16 @@ main = do
   testComp
 
 testEval = do
-  let s = (constI 1) :: Eval Int
+  let s = (const 1) :: Eval Int
       Eval s2 = add s s
-      Eval s3 = signal 0 (\s -> (add s $ constI 1, s))
+      Eval s3 = signal (const 0) (\s -> (add s $ const (1 :: Int), s))
   putStrLn $ show $ take 10 s2
   putStrLn $ show $ take 10 s3
 
 testComp = do
-  let s = (constI 1) :: Comp Int
+  let s = (const 1) :: Comp Int
       Comp s2 = add s s
-      Comp s3 = signal 0 (\s -> (add s $ constI 1, s))
+      Comp s3 = signal (const 0) (\s -> (add s $ const (1 :: Int), s))
       
   putStrLn $ show s2
   s2' <- reifyGraph s2
