8cae13814c3604c959f0681cec8d8992ca278739
Tom Schouten <tom@zwizwa.be>
2026-01-31 15:08:42 -0500

    approxEQ float comparison

--- hs/SynthTools/Misc.hs
+++ hs/SynthTools/Misc.hs
@@ -4,6 +4,9 @@
 
 module SynthTools.Misc where
 
+import Debug.Trace
+import Text.Printf
+
 -- Run length encoding with explicit 0 count at the beginning
 -- and end. E.g.
 -- [1,0,...] is [(0,0),(1,1),(n,0)]
@@ -34,14 +37,54 @@ pad len sig = sig' where
 
 
 -- An ad-hoc inexact Float equality test.
-floatEQ = floatEQ' 0.00001 0.0000001
+
+-- This isn't useful when the range of errors varies wildly due to
+-- data size.  See approxEQ comment.
+floatEQ = floatEQ' 0.00001 0.00001
 
 floatEQ' :: Float -> Float -> [Float] -> [Float] -> Bool
-floatEQ' relError no_nan_offset a b = (e / (norm a)) < 0.00001 where
-  n = fromIntegral $ length a
+floatEQ' relError no_nan_offset a b = trace' log $ (e / na) < relError where
+
+  log = "e=" ++ show e ++ ", na=" ++ show na
+  -- log = show (take 10 a, take 10 b)
+
+  na = norm a
+  
   sum = foldl (+) 0
   f x y = (x - y) ^ 2
-  e = (sqrt $ sum $ zipWith f a b) / n
+  e = (sqrt $ sum $ zipWith f a b)
   -- add a small offset to norm to avoid NaN when norm is 0.0f
-  norm x = no_nan_offset + (sqrt $ sum $ zipWith (*) x x) / n
+  norm x = no_nan_offset + (sqrt $ sum $ zipWith (*) x x)
 
+  -- For quick disable of tracing.
+  trace' _ v = v
+
+
+
+-- I used this to test equality of two FIR implementations in a
+-- QuickCheck test.  What worked in the end was to use existing
+-- meaningful FIR coefficients together with uniform noise input.
+-- That gives a reasonable output signal where an absolute error is
+-- able to be used for comparison.
+--
+-- Before I used too much parameterization making the input and FIR
+-- sizes too variable where it was really hard to qualify what a good
+-- error range would be for approximate equality.
+
+approxEQ :: Float -> [Float] -> [Float] -> Bool
+approxEQ max_error as bs = log rv where
+  -- Piece-wise absolute error bound check
+  rv = and rvs
+  rvs = zipWith withinBounds as bs
+  withinBounds a b = error a b <= max_error
+  error a b = abs $ a - b
+
+  -- Log in case of bound error
+  log rv = if rv then rv else trace log' rv
+  log' = concat $ map show' $ zip [(0::Int)..] $ zipWith logab as bs
+  logab a b = (withinBounds a b, error a b, a, b)
+  show' (n,(rv,e,a,b)) =
+    printf "%3d %+3.7f %+3.7f %+3.7f %s\n" n a b e $ show rv
+
+  -- For quick disable of tracing.
+  trace' _ v = v
